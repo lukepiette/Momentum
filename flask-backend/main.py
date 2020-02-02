1a33202893958b9bf019a8b1a5cf9f2457d06c12
piopio8000 <lukewpiette@gmail.com>
2020-02-02 17:36:20 -0500

    First commit

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -4,6 +4,6 @@ app = flask.Flask("__main__")
 
 @app.route("/")
 def my_index():
-    return flask.render_template("index.html", token="Momentum")
+    return flask.render_template("index.html", token="flask_token")
 
 app.run(debug=True)
